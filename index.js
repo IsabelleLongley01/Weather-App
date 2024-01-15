f41fc0d6091d53da113a2d72bb41360d0394f904
IsabelleLongley01 <121735301+IsabelleLongley01@users.noreply.github.com>
2024-01-15 12:55:45 +0000

    Adding forecast 1

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function search(event) {
   fetchCurrentWeather(city);
 }
 
-function fetchCurrentWeather(city); {
+function fetchCurrentWeather(city) {
   let apiKey = "ae82465odb0463ef60t6098c458a9b30";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(displayTemperature);
@@ -60,6 +60,36 @@ function displayTemperature(response) {
   humidityElement.innerHTML = `Humidity: ${response.data.temperature.humidity}%, `;
   windSpeedElement.innerHTML = `Wind Speed: ${response.data.wind.speed}km/h`;
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" id="icon">`;
+}
+
+function getForecast(city) {
+  let apiKey = "ae82465odb0463ef60t6098c458a9b30";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+  let days = ["Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+         <div class="weather-forecast-date">${day}</div>
+         <div class="weather-forecast-icon">☀️</div>
+         <div class="weather-forecast-temperatures">
+           <div class="weather-forecast-temperature">
+             <strong>15*</strong>
+           </div>
+           <div class="weather-forecast-temperature">9*</div>
+         </div>
+       </div>
+      `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
 
 let form = document.querySelector("#search-form");
